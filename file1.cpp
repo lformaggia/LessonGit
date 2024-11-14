c579f54d91b58324628362d1fac3e7d2591e4547
= <matte.campa@yahoo.it>
2024-11-14 19:46:00 +0100

    added a class to file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
 
+class ETL{
+  public:
+    ETL() { std::cout << "ETL constructor\n"; }
+    ~ETL();
+    void print();
+  private:
+    std::vector<int> v;
+};
+
 int main()
 {
   std::cout<<"Ciao Mamma, tutto bene, sto imparando git\n";
-  
+  auto etl = new ETL(); 
 }
 
